98902f2d6c205928912644a46d52fecc710183c4
Lindsey Morrison <lindseymorrison96@gmail.com>
2018-11-02 13:40:57 -0500

    Minor changes setting up user cick flow

--- app/src/main/java/com/example/isabelmangan/blueprinttravel/LoginActivity.java
+++ app/src/main/java/com/example/isabelmangan/blueprinttravel/LoginActivity.java
@@ -118,33 +118,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         //updateUI(currentUser);
     }
 
-    /**
-     * Creates user in firebase
-     * @param email users email
-     * @param password users password
-     */
-    public void createUser(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password).
-                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
 
-                        // ...
-                    }
-                });
-    }
 
     /**
      * Logs in existing user in firebase
@@ -178,7 +152,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
 
-    public static void updateUI(FirebaseUser currentUser) {
+    public void updateUI(FirebaseUser currentUser) {
+        Intent intent = new Intent (this, MapActivity.class);
+        startActivity(intent);
         //TODO: frontend- create updateUI method to go to home page for currently signed in user.
     }
 

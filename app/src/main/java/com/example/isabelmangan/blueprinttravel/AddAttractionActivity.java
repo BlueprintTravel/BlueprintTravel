80461df0e8bcbfd1b1e9d61bee99ccd98f391b54
Samantha Bravos <sambravos@Samanthas-MacBook-Pro-2.local>
2018-11-13 21:24:24 -0600

    Implemented duration picker using radio group - needs review

--- app/src/main/java/com/example/isabelmangan/blueprinttravel/AddAttractionActivity.java
+++ app/src/main/java/com/example/isabelmangan/blueprinttravel/AddAttractionActivity.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -30,9 +32,7 @@ public class AddAttractionActivity extends AppCompatActivity {
         String duration;
     }
 
-
     private static final String TAG = "MyAttraction";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,60 +68,58 @@ public class AddAttractionActivity extends AppCompatActivity {
             }
         });
 
-        //Set Required bool
-        //Set when add attraction button is clicked
-        // initiate a Switch
+        //Initiate a Switch for required state
         final Switch simpleSwitch = (Switch) findViewById(R.id.attraction_required_toggle);
         simpleSwitch.setChecked(false);
-        // check current state of a Switch (true or false).
-        //addAttraction.isReq = simpleSwitch.isChecked();
-        //switch test
-       //Log.d(TAG, "Required status: " + addAttraction.isReq);
 
+        //final RadioGroup rb = (RadioGroup) findViewById(R.id.radioGroup);
 
-        //Set Duration string
-        /**public void onRadioButtonClicked(View view) {
-            // Is the button now checked?
-            boolean checked = ((RadioButton) view).isChecked();
+        //Add Attraction Button
+        Button mAddAttractionButton = (Button) findViewById(R.id.add_attraction_button);
+        mAddAttractionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            //TODO: add attraction to list in database
+                //Set Required bool from Switch isChecked
+                addAttraction.isReq = simpleSwitch.isChecked();
 
-            // Check which radio button was clicked
-            switch(view.getId()) {
-                case R.id.radio_pirates:
-                    if (checked)
-                        // Pirates are the best
-                        break;
-                case R.id.radio_ninjas:
-                    if (checked)
-                        // Ninjas rule
-                        break;
+                //Test switch status
+                Log.d(TAG, "Required status: " + addAttraction.isReq);
+
+                //TODO: Set Duration from radio buttons
+                 RadioGroup rb = (RadioGroup) findViewById(R.id.radioGroup);
+                 rb.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+                    @Override
+                    public void onCheckedChanged(RadioGroup group, int checkedId) {
+                        //test checkedid
+                        Log.d(TAG, "CheckedID: " + checkedId);
+
+                        switch (checkedId) {
+                            case R.id.attraction_duration_short_radio:
+                                // to task
+                                addAttraction.duration = "short";
+                                break;
+                            case R.id.attraction_duration_medium_radio:
+                                // to task
+                                addAttraction.duration = "medium";
+                                break;
+                            case R.id.attraction_duration_long_radio:
+                                // to task
+                                addAttraction.duration = "long";
+                                break;
+                        }
+                    }
+                });
+
+                //Test duration result
+                Log.d(TAG, "Duration: " + addAttraction.duration);
+
+                //TODO: send database addAttraction object
+
+                //TODO: Call edit trip activity
+                //finishActivity(key);???
             }
-        } **/
-
-
-        //add attraction button takes user to edit trip activity
-
-         Button mAddAttractionButton = (Button) findViewById(R.id.add_attraction_button);
-         mAddAttractionButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-        //TODO: add attraction to list in database
-            //TODO: Set Required bool from Switch isChecked
-            addAttraction.isReq = simpleSwitch.isChecked();
-
-            //Change switch text color to red if switch == true
-            /**if (addAttraction.isReq == true){
-                simpleSwitch.setTextColor(Color.RED);
-            }**/
-
-            //Test switch status
-            Log.d(TAG, "Required status: " + addAttraction.isReq);
-
-            //TODO: Set duration from radio buttons
-
-
-        //TODO: Call edit trip activity
-        //finishActivity(key);???
-        }
         });
 
     }

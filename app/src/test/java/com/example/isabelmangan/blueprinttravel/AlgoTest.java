18f6fcd08631c51cd4473e77be730560685bb363
sunchitanand <sunchit.anand@gmail.com>
2018-11-15 22:13:48 -0600

    added test3

--- app/src/test/java/com/example/isabelmangan/blueprinttravel/AlgoTest.java
+++ app/src/test/java/com/example/isabelmangan/blueprinttravel/AlgoTest.java
@@ -27,4 +27,14 @@ public class AlgoTest {
 
         assertNull(Algo.generateOptimizedRoute(curr,open,close,walking_time,time_spent));
     }
+
+    public void test3(){
+        //all places visited
+        int[] open = {0,400,400,400};
+        int[] close = {0,750,660,1020};
+        int[][] walking_time = {{0, 30, 60, 120}, {30, 0, 30, 90}, {60, 30, 0, 60}, {120, 90, 60, 0}};
+        int[] time_spent = {0, 60, 60, 60};
+
+        assertNull(Algo.generateOptimizedRoute(curr,open,close,walking_time,time_spent));
+    }
 }
